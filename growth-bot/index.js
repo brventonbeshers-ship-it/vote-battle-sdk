380491bc1ad257ee9c4e14d1a01565e20b517370
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-13 03:19:41 +0300

    ci: add growth pulse workflow for SDK downloads

---
+++ growth-bot/index.js
@@ -0,0 +1,5 @@
+import * as sdk from 'vote-battle-sdk';
+
+console.log('Growth bot running for vote-battle-sdk...');
+console.log('SDK exports:', Object.keys(sdk).join(', '));
+console.log('Smoke check passed.');
